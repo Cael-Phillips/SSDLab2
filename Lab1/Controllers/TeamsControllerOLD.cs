ac6a477bb2b223c90b4ade64beecc6de9d41f205
Cael-Phillips <62359650+Cael-Phillips@users.noreply.github.com>
2022-10-09 12:49:51 -0400

    Add project files.

---
+++ Lab1/Controllers/TeamsControllerOLD.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab1.Data;
+using Lab1.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Lab1.Controllers {
+    public class TeamsControllerOLD : Controller {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsControllerOLD(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        // GET: Teams
+        public async Task<IActionResult> Index() {
+            return View(await _context.Team.ToListAsync());
+        }
+
+        // GET: Teams/Details/5
+        [Authorize(Roles = "Manager,Player")]
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null || _context.Team == null) {
+                return NotFound();
+            }
+
+            var team = await _context.Team
+                .FirstOrDefaultAsync(m => m.TeamID == id);
+            if (team == null) {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // GET: Teams/Create
+        [Authorize(Roles = "Manager")]
+        public IActionResult Create() {
+            return View();
+        }
+
+        // POST: Teams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize(Roles = "Manager")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TeamID,TeamName,Email,EstablishedDate")] Team team) {
+            if (ModelState.IsValid) {
+                _context.Add(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Edit/5
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> Edit(int? id) {
+            if (id == null || _context.Team == null) {
+                return NotFound();
+            }
+
+            var team = await _context.Team.FindAsync(id);
+            if (team == null) {
+                return NotFound();
+            }
+            return View(team);
+        }
+
+        // POST: Teams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> Edit(int id, [Bind("TeamID,TeamName,Email,EstablishedDate")] Team team) {
+            if (id != team.TeamID) {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid) {
+                try {
+                    _context.Update(team);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException) {
+                    if (!TeamExists(team.TeamID)) {
+                        return NotFound();
+                    }
+                    else {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(team);
+        }
+
+        // GET: Teams/Delete/5
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> Delete(int? id) {
+            if (id == null || _context.Team == null) {
+                return NotFound();
+            }
+
+            var team = await _context.Team
+                .FirstOrDefaultAsync(m => m.TeamID == id);
+            if (team == null) {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // POST: Teams/Delete/5
+        [Authorize(Roles = "Manager")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id) {
+            if (_context.Team == null) {
+                return Problem("Entity set 'ApplicationDbContext.Team'  is null.");
+            }
+            var team = await _context.Team.FindAsync(id);
+            if (team != null) {
+                _context.Team.Remove(team);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamExists(int id) {
+            return _context.Team.Any(e => e.TeamID == id);
+        }
+    }//class
+}//namespace
---
+++ Lab1/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab1.Models {
+    public class Team{
+        //ID
+        [Required]
+        [Display(Name = "Team ID")]
+        public int TeamID { get; set; }
+        //Team Name
+        [Required]
+        [Display(Name = "Team Name")]
+        public string? TeamName { get; set; }
+        //Email
+        [Required]
+        [Display(Name = "Email")]
+        public string? Email { get; set; }
+
+        //Established Date
+        [Display(Name = "Date Established")]
+        public DateTime EstablishedDate { get; set; }
+    }//class
+}//namespace
